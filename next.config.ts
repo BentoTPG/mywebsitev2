e0ac5b95bb5ffbc91a3e95c5511c84e254edce08
BentoTPG <130327391+BentoTPG@users.noreply.github.com>
2025-04-19 08:21:56 +0700

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-module.exports = {
-  allowedDevOrigins: ['http://192.168.1.25:3000'],
-}
-
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
