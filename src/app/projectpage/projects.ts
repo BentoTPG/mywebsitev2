7c3699c6d9f5fa61c152f625a930bfbe579060ee
BentoTPG <peem55383@gmail.com>
2025-04-27 21:06:46 +0700

    fix project image

--- src/app/projectpage/projects.ts
+++ src/app/projectpage/projects.ts
@@ -5,7 +5,7 @@ export const projects = [
         link: [{name: "Github",url: "https://github.com/BentoTPG/mywebsitev2"},{name: "Website",url: "https://bentotpg.github.io/mywebsitev2/"}],
         description: "Better one with framework",
         tags: ["Next js","Typescript","Tailwind"],
-        image: "/Projectpics/WebV2_Screenshot.png",
+        image: "./Projectpics/WebV2_Screenshot.png",
     },
     {
         name: "Old Portfolio Website",
@@ -13,7 +13,7 @@ export const projects = [
         link: [{name: "Github",url: "https://github.com/BentoTPG/Website"},{name: "Website",url: "https://bentotpg.github.io/Website/"}],
         description: "Make without framework. CSS by Bootstrap. Weird looking ahhh",
         tags: ["HTML","Javascript","Bootstrap"],
-        image: "/Projectpics/Web_Screenshot.jpg",
+        image: "./Projectpics/Web_Screenshot.jpg",
     },
     {
         name: "Disease Prediction (Cataract)",
@@ -21,7 +21,7 @@ export const projects = [
         link: [{name: "Github",url: "https://github.com/BentoTPG/cataract-test"},{name: "Website",url: "https://bentotpg.github.io/cataract-test/"}],
         description: "Cataract prediction using machine learning from Teachable Machine",
         tags: ["HTML", "Javascript","Teachable Machine"],
-        image: "/Projectpics/Cataract.png",
+        image: "./Projectpics/Cataract.png",
     },
     {
         name: "ESP8266-home-wifi-switch",
@@ -29,6 +29,6 @@ export const projects = [
         link: [{name: "Github",url: "https://github.com/BentoTPG/ESP8266-home-wifi-switch"}],
         description: "I use it every day and it still fine",
         tags: ["IOT","C++","ESP8266"],
-        image: "/Projectpics/ESP8266-switch.png",
+        image: "./Projectpics/ESP8266-switch.png",
     },
 ];
